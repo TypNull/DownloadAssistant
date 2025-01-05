9cead7a00186df2c54e3cd93347eecbafebd9443
Meyn <loads@gmx.de>
2025-01-05 23:30:27 +0100

    Fix invalid windows Filenames

--- DownloadAssistant/Utilities/IOManager.cs
+++ DownloadAssistant/Utilities/IOManager.cs
@@ -14,13 +14,13 @@ namespace DownloadAssistant.Utilities
         /// Array of characters that are not allowed in file names.
         /// </summary>
         public static char[] InvalidFileNameCharsWindows = new char[]
-          {
+        {
             '\"', '<', '>', '|', '\0',
             (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
             (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
             (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
-            (char)31, ':', '?', '\\', '/'
-          };
+            (char)31, ':', '?', '\\', '/', '*'
+        };
 
         public static char[] InvalidFileNameCharsUnix = new char[] { '/', '\0' };
 
@@ -43,10 +43,10 @@ namespace DownloadAssistant.Utilities
         {
             StringBuilder fileBuilder = new(name);
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                foreach (char c in InvalidFileNameCharsWindows)
+                foreach (char c in InvalidFileNameCharsWindows.Concat(Path.GetInvalidFileNameChars()))
                     fileBuilder.Replace(c.ToString(), string.Empty);
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                foreach (char c in InvalidFileNameCharsUnix)
+                foreach (char c in InvalidFileNameCharsUnix.Concat(Path.GetInvalidFileNameChars()))
                     fileBuilder.Replace(c.ToString(), string.Empty);
             return fileBuilder.ToString();
         }
